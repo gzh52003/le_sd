fab39824dc9813ca13380cbe1d3068f00bdfcd30
hongge <1226514725@qq.com>
2020-09-22 16:41:50 +0800

    简化

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withRouter } from 'react-router-dom';
+import React from 'react'
 
-// @withRouter
+function App() {
+  return (
+    <div className="App">
 
-class App extends React.Component {
-  state = {
-    num: 10
-  }
-  changeNumber = () => {
-    this.setState({
-      num: this.state.num + 1
-    })
-  }
-  render() {
-    console.log('App.props', this.props);
-    return (
-      <div className='App'>
-        叼你嗨啊 <br />
-        SEVEN HEAD<br />
-        {this.state.num}<br />
-        <button onClick={this.changeNumber}>叼多一次</button>
-      </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default App;
+export default App
